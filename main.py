38c6692d9447d5282685b7f0e50818365264a1e5
Pavlo Aivazov <72413229+paivazov@users.noreply.github.com>
2023-06-19 16:10:39 +0300

    refactor: change incoming param in path creating util (#2)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 @app.post("/convert")
 def convert_image(output_format: str, image: UploadFile = File()):
     with Image.open(image.file) as logo:
-        image_data = create_filename_path(image, output_format)
+        image_data = create_filename_path(image.filename, output_format)
         headers = {'Content-Disposition': f'attachment; filename="{image_data["name"]}"'}
         full_path = image_data["full_path"]
         logo.convert('RGB').save(full_path, format=output_format)
--- utils.py
+++ utils.py
@@ -2,12 +2,10 @@ import re
 import os
 import uuid
 
-from fastapi import UploadFile
 
-
-def create_filename_path(image: UploadFile, output_format: str) -> dict:
+def create_filename_path(image_name: str, output_format: str) -> dict:
     dirname = os.getcwd()
     unique_code = str(uuid.uuid4())[:8]
-    filename = re.match(r"(.*)\.\w+", image.filename).group(1)
+    filename = re.match(r"(.*)\.\w+", image_name).group(1)
     converted_filename = f"{filename}_{unique_code}.{output_format}"
     return {"name": converted_filename, "full_path": os.path.join(dirname, "converted_images", converted_filename)}
